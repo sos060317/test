7e3347a849ab075c1b9bfec1c548ff23d5a565d4
WooJiWon <woojiwon060317@gmail.com>
2024-04-25 20:09:20 +0900

    test1 커밋입니다.

---
+++ Assets/TestFolder1/Test1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("test1¿‘¥œ¥Ÿ.");       
+    }
+}
